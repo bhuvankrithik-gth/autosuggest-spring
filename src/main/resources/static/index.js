97e91d5cdf575ee9676d7e5c6e2530e06ac09c7b
ANUMALASETTY BHUVAN KRITHIK <bhuvankeerthan@gmail.com>
2026-07-18 11:50:56 +0530

    mm

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,2 +1,26 @@
 // alert("this web might be malicious")
 console.log("hello from js ");
+
+var users=[
+    {
+        "name":"john doe",
+        "gender":"male",
+        "img":"john.png"
+
+    },
+    {
+        "name":"jane doe",
+        "gender":"female",
+        "img":"jane.png"
+    }
+]
+var id = 0;
+function toggleuser(){
+    id=(id+1)%2;
+}
+var userImage=document.getElementById("user-image");
+userImage.src= users[id].img;
+var userName=document.getElementById("user-name");
+userName.innerHTML= users[id].name;
+var userGender=document.getElementById("user-gender");   
+userGender.innerHTML= users[id].gender;
